4ca330a36bdf1818df7930e9c0d693e44d12b0ba
xiirx <nbhk2016@ya.ru>
2020-10-05 22:49:50 +0300

    correct

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function main(){
 	//pannellum.viewer('panorama', await fetch('panoramas/test.json'));
-	console.log(await fetch('panoramas/test.json'))
+	console.log((await fetch('panoramas/test.json').json())
 }
 
 main()
